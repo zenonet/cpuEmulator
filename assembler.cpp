c1b242f95b58a3a2c47db3557cfc87a48a8e312f
zenonet <78077158+zenonet@users.noreply.github.com>
2024-08-23 19:49:58 +0200

    Continued parser for assembler and made mov instruction working in assembler

--- assembler.cpp
+++ assembler.cpp
@@ -1,8 +1,17 @@
+#include <cstdint>
 #include <cstdio>
 #include <fstream>
 #include <iostream>
 #include <stdio.h>
 #include <string>
+
+int asRegister(std::string arg){
+    if(arg[0] != 'r'){
+        std::cout << "Error: Expected register reference but got '" << arg << "'\n";
+        exit(1);
+    }
+    return std::stoi(arg.substr(1));
+}
 
 int main(){
     std::ifstream sourceStream("first.zasm");
@@ -11,8 +20,43 @@ int main(){
         std::string line;
         std::getline(sourceStream, line);
         
-        std::cout << "Working on instruction: " << line << '\n';
 
-        binStream << "Was ist denn bei Karsten los?\n";
+        int firstSpace = line.find(' ');
+        int secondSpace = line.find(' ', firstSpace+1);
+        std::string keyword = line.substr(0, firstSpace);
+        std::string arg1 = line.substr(firstSpace+1, secondSpace-firstSpace);
+        std::string arg2 = line.substr(secondSpace+1);
+
+        std::cout << "Keyword: '" << keyword << "'\n";
+
+        std::cout << "Arg1: " << arg1 << '\n';
+        std::cout << "Arg2: " << arg2 << '\n';
+
+        if(keyword == "init"){
+            
+            int reg = asRegister(arg1);
+            int value = std::stoi(arg2);
+            std::cout << "Initializing register " << reg << " with value " << value <<  '\n';
+
+        }
+        else if(keyword == "mov"){
+            char destReg = (char) asRegister(arg1);
+            char srcReg = (char) asRegister(arg2);
+
+            char firstByte;
+            char secondByte;
+
+            firstByte = (0b0010 << 4) | destReg;
+            secondByte = srcReg << 4;
+
+            binStream << firstByte;
+            binStream << secondByte;
+            std::cout << "Instruction bytes: " << (int)firstByte << ", " << (int) secondByte << '\n';
+            std::cout << "Moving value from register " << srcReg << " to register " << destReg << '\n';
+        }
+        else{
+            std::cout << "Error: Keyword '" << keyword << "' is not defined\n";
+            exit(1);
+        }
     }
 }
